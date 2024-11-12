0cbd8922c27a00f92ac306c07b74f51bbc50b3ee
piriri1000 <bags98021@gmail.com>
2024-11-12 19:29:37 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+function ViewPlace(){
+    let tagArray = document.getElementsByClassName("place")
+    for(let i=0; i<tagArray.length; i++){
+        let tag = tagArray[i];
+        if(tag.style.color!=="blue"){
+            tag.style.color="blue"
+            tag.style.fontSize = "20px"
+        }
+        else{
+            tag.style.color = "black"
+            tag.style.fontSize ="16px"
+        }
+    }
+}
+function EatFood(){
+    let tagArray = document.getElementsByClassName("food")
+    for(let i=0; i<tagArray.length; i++){
+        let tag = tagArray[i];
+        if(tag.style.color!=="red"){
+            tag.style.color="red"
+            tag.style.fontSize = "20px"
+        }
+        else{
+            tag.style.color = "black"
+            tag.style.fontSize ="16px"
+        }
+    }
+}
